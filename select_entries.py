9dab2cdf3c74d318c166d31239dc75c8b177d48f
Keijukainen311 <b.veenaas@web.de>
2023-03-09 21:02:18 +0100

    fix in tumbeling window...

--- select_entries.py
+++ select_entries.py
@@ -17,7 +17,8 @@ def get_sliding_window(r, stream_name, window_size):
 
 def get_tumbling_window(r, stream_name, window_size):
     latest_id = r.xinfo_stream(stream_name)['last-entry'][0]
-    oldest_id = str(int(latest_id) - (window_size - 1))
+    latest_entry_id = latest_id.split("-")[0]
+    oldest_id = str(int(latest_entry_id) - (window_size - 1))
     entries = r.xrange(stream_name, oldest_id, latest_id)
     window_entries = []
     for id, fields in entries:
@@ -25,10 +26,11 @@ def get_tumbling_window(r, stream_name, window_size):
     return window_entries
 
 r = redis_connector.connect()
-last = get_last_n_entries(r, "mubea_trb", 10)
+#last = get_last_n_entries(r, "mubea_trb", 10)
 
-#slide = get_sliding_window(r, "mubea_trb", 10)
+slide = get_sliding_window(r, "mubea_trb", 5)
+#print(slide)
 
 
-#tumble = get_tumbling_window(r, "mubea_trb", 10)
-#print (tumble)
+tumble = get_tumbling_window(r, "mubea_trb", 10)
+print (tumble)
